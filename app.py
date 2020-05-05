ddf4dbb6c1a9057236ea8fd38e7a66851131d3b7
Jp889 <jp889@cornell.edu>
2020-05-05 22:13:47 +0700

    create files with basic layou

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import dao
+import json
+from db import db
+
+# define dp
+app = Flask(__name__)
+db_filename = "carShare.db"
+
+# config
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+# initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
